4bb74a8654a54e7ffa91becf00967c36dd1d8b12
Lyvia Boutin <lyviaboutin@gmail.com>
2023-04-25 05:31:58 -0300

    Feat: Resolve Requisito 07

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
+  state = {
+    expensesRow: [],
+  };
+
+  componentDidUpdate(prevState) {
+    const { arrayExpenses } = this.props;
+    if (prevState.arrayExpenses !== arrayExpenses) {
+      this.mapTable();
+    }
+  }
+
+  mapTable = () => {
+    const { arrayExpenses } = this.props;
+    const mapped = arrayExpenses.map((expense) => {
+      console.log('oi');
+      const convertedValue = expense.value * expense.exchangeRates[expense.currency].ask;
+      return (
+        <tr key={ expense.id }>
+          <td>{expense.description}</td>
+          <td>{expense.tag}</td>
+          <td>{expense.method}</td>
+          <td>{parseFloat(expense.value).toFixed(2)}</td>
+          <td>{expense.exchangeRates[expense.currency].name}</td>
+          <td>{parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+          <td>{convertedValue.toFixed(2)}</td>
+          <td>Real</td>
+        </tr>
+      );
+    });
+    this.setState({
+      expensesRow: mapped,
+    });
+  };
+
   render() {
+    const { expensesRow } = this.state;
     return (
       <div>
         <table>
@@ -18,10 +55,22 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expensesRow}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  arrayExpenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  arrayExpenses: PropTypes
+    .arrayOf(PropTypes.shape({ id: PropTypes.number.isRequired })).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
